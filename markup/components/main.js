4efe3b6aeeb84cc4bde7de84c856f191bf5e1233
maxswim228 <maxswim228@gmail.com>
2018-08-30 16:17:27 +0600

    00001110001100100010100001001

--- markup/components/main.js
+++ markup/components/main.js
@@ -16,6 +16,9 @@ $('#my-form').validate({
         },
         password: {
             required: true,            
+        },
+        passwordYet: {
+            required: true,            
         }
     },
 
@@ -27,6 +30,8 @@ $('#my-form').validate({
     },
 
 });
+
+
 
 $('#my-form-2').validate({
     rules: {
@@ -76,13 +81,14 @@ $(".accordion").accordion ({
     hide: { effect: "fade", duration: 150 },
     show: { effect: "fade", duration: 150 },
     heightStyle: "content",
+    collapsible: true,
 });
 
 $(function () {
 
-    $('.popup_close, .overlay').on('click', function (event) {
+    $('.popup_close, .overlay, .humburger-overlay').on('click', function (event) {
         $('body').css('overflow', 'auto');
-        $('.popup, .overlay, .header-menu').removeClass('visible');
+        $('.popup, .overlay, .header-menu, .humburger-overlay').removeClass('visible');
 
     });
 
@@ -103,12 +109,12 @@ $(function () {
 });
 
 $('.menu-open').click(function() {
-    $('.header-menu, .overlay').addClass("visible");
+    $('.header-menu, .humburger-overlay').addClass("visible");
     $('body').css('overflow', 'hidden');
 });
 
 $(".close-menu").click(function() {
-    $(".header-menu, .overlay").removeClass("visible");
+    $(".header-menu, .humburger-overlay").removeClass("visible");
     $('body').css('overflow', 'auto');
 });
 
@@ -118,17 +124,182 @@ $('.popup-reg-open').click(function() {
 
 $('.popup-sign-open').click(function () {
     $(".popup-reg").removeClass('visible');
-    $(".popup-sign").addClass('visible')
-    
+    $(".popup-sign").addClass('visible'); 
 });
 
 $('.main-block table').each(function () {
     $(this).wrap('<div class="main-table-container"></div>');
 });
 
-mobiscroll.settings = {
-    theme: 'ios',
-    lang: 'en'
-};
 
-var now = new Date();
+$('.myChart').each(function(){
+    var it = $(this);
+
+    var ctx = it;
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for ou  r dataset
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                data: [20,24,26,26,30,30,30,26,36,32,33,32,32,31,32,33,32,32,32,30,34,30,31,32,33],
+                backgroundColor: 'transparent',
+                borderWidth: 1,
+                borderColor: '#1b203f',
+                pointBackgroundColor: "#488af3",
+                pointRadius: 2,
+                pointBorderColor: "#488af3",
+            }],
+            
+        },
+        // Configuration options go here
+        options: {
+            responsive: true,
+            legend: {
+                display: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            lineTension: true,
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+});
+
+$('.myChartMulti').each(function(){
+    var it = $(this);
+
+    var ctx1 = it;
+    var chart = new Chart(ctx1, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for ou  r dataset
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    data: [20,24,26,26,30,30,30,26,36,32,33,32,32,31,32,33,32,32,32,30,34,30,31,32,33],
+                    backgroundColor: 'transparent',
+                    borderWidth: 1,
+                    borderColor: '#1b203f',
+                    pointBackgroundColor: "#488af3",
+                    pointRadius: 2,
+                    pointBorderColor: "#488af3",
+                },{
+                    data: [18,22,24,24,28,28,28,24,20,30,31,30,30,29,30,31,30,30,30,28,32,28,29,30,31],
+                    backgroundColor: 'transparent',
+                    borderColor: '#6c1515',
+                    pointRadius: 2,
+                    borderWidth: 1,
+                    pointBackgroundColor: "#e92e2e",
+                    pointBorderColor: "#e92e2e",
+                },{
+                    data: [16,20,22,22,26,26,26,22,10,28,29,28,28,27,28,29,28,28,28,26,30,26,27,28,29],
+                    backgroundColor: 'transparent',
+                    pointRadius: 2,
+                    borderWidth: 1,
+                    borderColor: '#167641',
+                    pointBackgroundColor: "#19c466",
+                    pointBorderColor: "#19c466",
+                }
+            ],
+            
+        },
+
+        // Configuration options go here
+        options: {
+            legend: {
+                display: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            lineTension: true,
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+});
+
+
+$('.myChartPie').each(function(){
+    var it = $(this);
+
+    var ctxPie = it;
+    var myPieChart = new Chart(ctxPie, {
+        // The type of chart we want to create
+        type: 'pie',
+        
+        segmentShowStroke: false,
+        segmentStrokeWidth: 0,
+
+        // The data for ou  r dataset
+			data: {
+				datasets: [{
+					data: [
+                        7,11,23,10,16,17,57
+                    ],
+
+                    borderWidth: 0,
+
+                    backgroundColor: [
+                        '#00a2d0',
+                        '#48bdf3',
+                        '#093479',
+                        '#3460a6',
+                        '#488af3',
+                        '#2859a9',
+                        '#12739c'
+                    ], 
+                }],
+                
+                labels: [
+                    'Россия',
+                    'США',
+                    'Канада',
+                    'Франция',
+                    'Испания',
+                    'англия',
+                    'барсик'
+                ],
+                 
+            },
+            
+			options: {
+                legend: {
+                    display: true,
+                    position: 'right',
+                }
+                
+                
+			}
+    });
+
+});
