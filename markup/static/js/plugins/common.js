3dafe282612c9fd59d1a0f358ccce69e749b2328
maxswim228 <maxswim228@gmail.com>
2018-09-24 23:39:38 +0600

    commitfdsafsfdsafsda

--- markup/static/js/plugins/common.js
+++ markup/static/js/plugins/common.js
@@ -31,21 +31,46 @@ $('#my-form').validate({
 
 });
 
-$('#form-already').validate({
+
+$("#form-news").validate({
     rules: {
-        url: {
-            required: true,            
-        }
+        email: {
+            required: true,
+            email: true,
+        },
+        name: {
+            required: false,
+        },
     },
 
     errorPlacement: function (error, element) {},
 
     submitHandler: function() {
-        $("#form-already").css("display", "none");
-        $("#form-desc").css("display", "flex");
+        $(this).trigger('reset');
+        alert("Ожидайте звонка!");
     },
 
 });
+
+$('.form-already').each(function() {
+    $(this).validate({
+        rules: {
+            url: {
+                required: true,            
+            }
+        },
+    
+        errorPlacement: function (error, element) {},
+    
+        submitHandler: function() {
+            $("#form-already").css("display", "none");
+            $("#form-desc").css("display", "flex");
+        },
+    
+    });
+});
+
+
 
 function copy(str){
     let tmp   = document.createElement('INPUT'),
@@ -171,7 +196,6 @@ $('.main-block table').each(function () {
     $(this).wrap('<div class="main-table-container"></div>');
 });
 
-
 $('.myChart').each(function(){
     var it = $(this);
 
@@ -182,16 +206,17 @@ $('.myChart').each(function(){
 
         // The data for ou  r dataset
         data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July"],
+            labels: ["авг", "сен", "окт", "ноя", "дек", "2018", "фев", "мар", "апр", "май", "23", "24", "25", "26", "27", "28", "29", "30", "31", "июн", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+
             datasets: [{
-                data: [20,24,26,26,30,30,30,26,36,32,33,32,32,31,32,33,32,32,32,30,34,30,31,32,33],
+                data: [20,24,26,26,30,30,30,26,36,32,33,32,32,31,32,33,32,32,32,30,34,30,31,32,33,30,34,30,31,32,33],
                 backgroundColor: 'transparent',
                 borderWidth: 1,
                 borderColor: '#1b203f',
                 pointBackgroundColor: "#488af3",
                 pointRadius: 2,
                 pointBorderColor: "#488af3",
-            }],
+            },],
             
         },
         // Configuration options go here
@@ -228,13 +253,14 @@ $('.myChartMulti').each(function(){
     var chart = new Chart(ctx1, {
         // The type of chart we want to create
         type: 'line',
+        cubicInterpolationMode: 0, 
 
         // The data for ou  r dataset
         data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July", "February", "March", "April", "May", "June", "July"],
+            labels: ["авг", "сен", "окт", "ноя", "дек", "2018", "фев", "мар", "апр", "май", "23", "24", "25", "26", "27", "28", "29", "30", "31", "июн", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
             datasets: [
                 {
-                    data: [20,24,26,26,30,30,30,26,36,32,33,32,32,31,32,33,32,32,32,30,34,30,31,32,33],
+                    data: [20,24,26,26,30,30,30,26,36,32,33,32,32,31,32,33,32,32,32,30,34,30,31,32,33,30,34,30,31,32,33],
                     backgroundColor: 'transparent',
                     borderWidth: 1,
                     borderColor: '#1b203f',
@@ -242,7 +268,7 @@ $('.myChartMulti').each(function(){
                     pointRadius: 2,
                     pointBorderColor: "#488af3",
                 },{
-                    data: [18,22,24,24,28,28,28,24,20,30,31,30,30,29,30,31,30,30,30,28,32,28,29,30,31],
+                    data: [18,22,24,24,28,28,28,24,20,30,31,30,30,29,30,31,30,30,30,28,32,28,29,30,31,30,34,30,31,32,33],
                     backgroundColor: 'transparent',
                     borderColor: '#6c1515',
                     pointRadius: 2,
@@ -250,7 +276,7 @@ $('.myChartMulti').each(function(){
                     pointBackgroundColor: "#e92e2e",
                     pointBorderColor: "#e92e2e",
                 },{
-                    data: [16,20,22,22,26,26,26,22,10,28,29,28,28,27,28,29,28,28,28,26,30,26,27,28,29],
+                    data: [16,20,22,22,26,26,26,22,10,28,29,28,28,27,28,29,28,28,28,26,30,26,27,28,29,30,34,30,31,32,33],
                     backgroundColor: 'transparent',
                     pointRadius: 2,
                     borderWidth: 1,
